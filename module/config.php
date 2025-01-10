be029df838e8a467215fb882c7039e894928d29a
ying2368 <105878980+ying2368@users.noreply.github.com>
2025-01-10 18:28:12 +0800

    Update config.php

--- module/config.php
+++ module/config.php
@@ -1,9 +1,9 @@
 <?php
 // 設定資料庫連線
 define('DB_HOST', 'localhost');
-define('DB_USER', '1111442');
-define('DB_PASS', '001019');
-define('DB_NAME', 'final_project_v1');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
